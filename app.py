d844b5a2cf868221592ac02dbcdb9158ce53606d
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-08-14 02:36:22 +0800

    tes

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def get_rooms():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)  # Jalankan aplikasi Flask
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Jalankan aplikasi Flask
